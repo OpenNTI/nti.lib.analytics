2d1625f14fa3afca010c955d00ccae5dedebbab3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-03-24 14:51:55 -0500

    node-uuid => uuid

--- src/models/abstract/Base.js
+++ src/models/abstract/Base.js
@@ -1,8 +1,7 @@
-import uuid from 'node';
+import uuid from 'uuid';
 
 import {UNKNOWN_TYPE, isKnown} from '../../MimeTypes';
 import HeartbeatManager from '../../utils/Heartbeat';
-
 import {isFunction, definePublic, defineProtected, updateValue} from '../../utils';
 
 const seconds = (startTime = Date.now(), endTime = Date.now()) => Math.round((endTime - startTime) / 1000);
