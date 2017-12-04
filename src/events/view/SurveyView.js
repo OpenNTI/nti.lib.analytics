63078c16ecfd40d7670b6a650a7392179a0d581a
Jonathan Grimes <jsg2021@gmail.com>
2017-12-04 17:10:55 -0600

    NTI-3992 - add SurveyView

---
+++ src/events/view/SurveyView.js
@@ -0,0 +1,16 @@
+import Timed from '../abstract/Timed';
+import {register} from '../Registry';
+
+@register('SurveyView')
+export default class SurveyView extends Timed {
+	static EventType = 'application/vnd.nextthought.analytics.surveyviewevent'
+
+	getData () {
+		const data = super.getData();
+
+		return {
+			...data,
+			ContentId: this.data.ContentID
+		};
+	}
+}
